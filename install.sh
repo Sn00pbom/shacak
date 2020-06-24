ea9715339523a738c8cc8dd580a3c6f0a3b3f868
Zach Lefin <zachlefin@gmail.com>
2020-06-24 15:37:31 -0400

    add install copy or symlink options, fix path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true;
 do
-    read -p "Copy all files in $PWD/home/ to $HOME/ ? [y/n]" yn
+    read -p "Dump all files in $PWD/home/ to $HOME/ ? [y/n] " yn
     case $yn in
 	y)
 	    break
@@ -12,9 +12,29 @@ do
     esac
 done
 
+while true;
+do
+    read -p "Copy files[0] or symbolic link files[1]? " mode
+    case $mode in
+	0 | 1)
+	    break
+	    ;;
+    esac
+done
+
+
 cd home
 for name in $(find . -type f)
 do
-	echo "$name -> $HOME/$name"
-	cp --remove-destination $name "$HOME/$name"
+    abs_source=${name/\./$PWD}
+    abs_dest=${name/\./$HOME}
+    echo "$abs_source -> $abs_dest"
+    case $mode in
+	0)
+	    cp --remove-destination $abs_source $abs_dest
+	    ;;
+	1)
+	    ln -sf $abs_source $abs_dest
+	    ;;
+    esac
 done
